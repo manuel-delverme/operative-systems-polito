349d46b5fe29e67990921fe461ca5603ff04d2a6
Isaac <isaac@bo>
2014-11-14 15:05:06 +0100

    brb work

--- ex6/ex6x1v2.c
+++ ex6/ex6x1v2.c
@@ -5,21 +5,42 @@
 int main(int argc, char *argv[]){
     char *fileName;
     FILE *execFile;
-    int n,i;
-    char row[500][5000];
-
+    int n;
+    char *row;
+    char *tokens;
+    pid_t pid;
+   
     if(argc != 3)
 	exit(1);
     fileName = argv[1];
     execFile  = fopen(fileName,"r"); 
     n =  atoi(argv[2]);
 
-    while( fgets(row,sizeof(row[0]),execFile)){
-    	execvp(row,"");
+    while(NULL != fgets(row,sizeof(row),execFile)){
+
+    	printf("pre %s\n", row);
+
+
+	for (int i = 0; i < len; i++) {
+	    if (str[i] == '\0') {
+		printf("'\\0'");
+	    } else {
+		printf("%c", str[i]);
+	    }
+	}
+	tokens =  strtok(row, " ");
+	while (tokens) {
+	    printf("Token: %s\n", tokens);
+	    tokens = strtok(NULL, " ");
+	}
+    	//printf("tok %s\n", tokens);
+/*   	pid = fork();
+	if( 0 == pid){
+    		row[] = NULL;
+ 		execvp(row);
+	}
 	sleep(n);
+*/
     }
-    row[i] = (char*) 0;
-    execvp(row);
     return 0;
 }
-
