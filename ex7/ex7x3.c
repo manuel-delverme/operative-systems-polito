27fdb5d0d56df2fd0a19abf853ecea4babcb1aa5
Manuel Del verme <s186541@labinf32.(none)>
2014-11-21 13:29:32 +0100

    x3 almost done, needs debug

--- ex7/ex7x3.c
+++ ex7/ex7x3.c
@@ -5,37 +5,52 @@
 
 void sig_handler(int sig_code);
 
-
 int main(int argc, char *argv[]){
-    pid_t pid;
-    pid_t ppid;
+    pid_t producer;
+    pid_t consumer;
+    char *fileName = "/tmp/qwpeo1o239021eiofile";
+    FILE *communication_file;
+    char row[100];
+    row[0] = "\0";
     
     signal(SIGCONT,sig_handler);
 
-    pid = fork();
-    pid = fork();
-    pid = fork();
-    while(1){
-	if(pid != 0){
-		printf("I'm the parent of %d\n",pid);
-		//fflush(stdout);
-		sleep(1);
-		//printf("[p] killing %d\n",pid);
-		kill(pid,SIGCONT);
-		//printf("[p] waiting for %d\n",pid);
-		pause();
-	} else {
-		pause();
-		printf("[c] I'm the child\n");
-		//fflush(stdout);
-		ppid = getppid();
-		sleep(1);
-		//printf("[c] killing %d\n",ppid);
-		kill(ppid,SIGCONT);
+    consumer = fork();
+    producer = fork();
+
+    if(consumer < 0) exit(1);
+    if(producer < 0) exit(1);
+
+    if( 0 == producer){
+    	pause();
+    	communication_file  = fopen(fileName,"w"); 
+	while( "end" != row){
+	    fscanf(stdin,"%s",&row);
+	    printf("%s",row);
+	    fwrite(row,sizeof(char),sizeof(row),communication_file);
+	    fflush(communication_file);
+	    kill(consumer,SIGCONT);
+	    pause();
 	}
+    } else if ( 0 == consumer) {
+    	communication_file  = fopen(fileName,"r"); 
+	kill(producer,SIGCONT);
+	while("end" != row){
+	    kill(consumer,SIGCONT);
+	    pause();
+	    fgets(row,sizeof(row),communication_file);
+	    printf("%s \n",row);
+	    kill(producer,SIGCONT);
+	}
+    } else {
+	waitpid(producer);
+	waitpid(consumer);
     }
+    return 0;
 }
 
 void sig_handler(int sig_code){
-	return;
+	if(SIGCONT == sig_code){
+		return;
+	}
 }
