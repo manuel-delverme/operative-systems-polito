7efc5e579bd8891f1989b02f40332c8f64cb7c3e
Manuel Del verme <s186541@labinf32.(none)>
2014-11-21 12:43:09 +0100

    ex7 1/2

---
+++ ex7/ex7x2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sig_handler(int sig_code);
+
+
+int main(int argc, char *argv[]){
+    pid_t pid;
+    pid_t ppid;
+    
+    signal(SIGCONT,sig_handler);
+
+    pid = fork();
+    while(1){
+	if(pid != 0){
+		printf("I'm the parent of %d\n",pid);
+		//fflush(stdout);
+		sleep(1);
+		//printf("[p] killing %d\n",pid);
+		kill(pid,SIGCONT);
+		//printf("[p] waiting for %d\n",pid);
+		pause();
+	} else {
+		pause();
+		printf("[c] I'm the child\n");
+		//fflush(stdout);
+		ppid = getppid();
+		sleep(1);
+		//printf("[c] killing %d\n",ppid);
+		kill(ppid,SIGCONT);
+	}
+    }
+}
+
+void sig_handler(int sig_code){
+	return;
+}
